969deb914501e7ac3ac108aa2b7e54d2260d70a3
kanbujianwojpg <1468057882@qq.com>
2020-08-17 14:50:38 +0800

    '入口配置'

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+// 处理客户端请求post参数
+// for parsing application/json
+app.use(express.json());
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//配置后端cors跨域
+app.use(cors());
 
 app.listen(6666, () => {
   console.log('running...');
 });
 
 app.get('/user', (req, res) => {
-  console.log(res);
+  console.log('hi');
 });
