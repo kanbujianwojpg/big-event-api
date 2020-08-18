a3cf9be5511cf04a69b0d4d6a08bad974c8f7e27
kanbujianwojpg <1468057882@qq.com>
2020-08-18 14:52:39 +0800

    更新用户密码实现'

--- routers/my.js
+++ routers/my.js
@@ -9,6 +9,7 @@ const router = express.Router();
 const utility = require('utility');
 const db = require(path.join(__dirname, '../common/db.js'));
 
+//获取用户信息
 router.get('/userinfo', async (req, res) => {
   // console.log(req.user);
   //获取token中的用户信息
@@ -26,6 +27,32 @@ router.get('/userinfo', async (req, res) => {
     res.json({
       status: 1,
       message: '获取用户数据失败',
+    });
+  }
+});
+
+//修改密码
+router.post('/updatepwd', async (req, res) => {
+  //获取请求参数
+  let id = req.user.id;
+  let params = req.body;
+  //2.对密码进行加密
+  params.oldPwd = utility.md5(params.oldPwd);
+  params.newPwd = utility.md5(params.newPwd);
+
+  //更新数据库信息(根据原密码更新新密码)
+  let sql = 'update user set password = ? where id = ? and password = ?';
+  let ret = await db.operateDb(sql, [params.newPwd, id, params.oldPwd]);
+  if (ret && ret.affectedRows > 0) {
+    res.json({
+      //更新成功
+      status: 0,
+      message: '修改密码成功',
+    });
+  } else {
+    res.json({
+      status: 1,
+      message: '修改密码失败',
     });
   }
 });
